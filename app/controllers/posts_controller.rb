f81ed3b58d220f0b5ba879813c529ceb69a66b3c
Paul_Beckwith <303dog@gmail.com>
2020-09-10 15:10:31 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,4 +26,10 @@ class PostsController < ApplicationController
 		@post.update(params.require(:post).permit(:title))
 		redirect_to post_path(@post)
 	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :description)
+	end
 end
